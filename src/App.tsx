28b7e809a21625361a3bec2eab8e0e1076790c6a
Luis Alberto Huaman <huamanluis808@gmail.com>
2024-08-22 22:13:34 -0500

    realizando ajustes basicos
    finalizando el proyecto

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import PatienForm from './components/PatienForm'
+import PatiensList from './components/PatiensList'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
 function App() {
-  return (
-    <>
-      <h1>Hola mundo</h1>
-    </>
-  );
+   return (
+      <>
+         <div className="container mx-auto mt-20">
+            <h1 className="font-black text-5xl text-center md:w-2/3 md:mx-auto">
+               Seguimiento Pacientes {''}
+               <span className="text-indigo-700">Veterinaria</span>
+            </h1>
+
+            <div className="mt-12 md:flex">
+               <PatienForm />
+               <PatiensList />
+            </div>
+         </div>
+         <ToastContainer />
+      </>
+   )
 }
 
-export default App;
+export default App
